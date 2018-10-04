e18afb44e4faffbd77baf21e042b3838a78a1f1c
Tan Avery <altan@ualberta.ca>
2018-10-04 17:37:38 -0600

    gameStatus now computed everytime the UI renders and a game status is now returned and shown at the bottom of the UI

--- NativityApp/src/components/Game.js
+++ NativityApp/src/components/Game.js
@@ -18,24 +18,47 @@ export default class Game extends Component {
     randomNumberCount: PropTypes.number.isRequired,
   };
   state = {
-    selectedNumbers: [0,4],
+    selectedIds: [],
   };
+  //our psuedo-randomly generated numbers array
   randomNumbers = Array
     .from({ length: this.props.randomNumberCount })
     .map(() => 1 + Math.floor(10 * Math.random()));
+  //the target we are trying to sum up to
   target = this.randomNumbers
     .slice(0, this.props.randomNumberCount - 2)
     .reduce((acc, curr) => acc + curr, 0); //this is easiest way to sum array. Starting from zero is a fail safe in case the array is empty
   
   isNumberSelected = (numberIndex) => {
-    return this.state.selectedNumbers.indexOf(numberIndex) >= 0;
+    return this.state.selectedIds.indexOf(numberIndex) >= 0;
   }
   selectNumber = (numberIndex) => {
     this.setState((prevState) => ({
-      selectedNumbers: [...prevState.selectedNumbers, numberIndex],
+      selectedIds: [...prevState.selectedIds, numberIndex],
     }));
   };
+  //gameStatus computes the status of the game[PLAYING, WON, LOST]
+  //at any time during the game. It's a function and not part of state
+  //because it is computed from the selectedIds array which IS
+  //stored in state
+  //TODO: Learn reduce inside out and other common use-cases
+  gameStatus = () => {
+    const sumSelected = this.state.selectedIds.reduce((acc, curr) => {
+      return acc + this.randomNumbers[curr];
+    }, 0);
+    if (sumSelected < this.target) {
+      return 'PLAYING';
+    }
+    if (sumSelected === this.target) {
+      return 'WON';
+    }
+    else {
+      return 'LOST';
+    } 
+    console.warn(sumSelected);
+  }
   render() {
+    const gameStatus = this.gameStatus();
     return (
       <View style={styles.container}>
         <Text style={styles.target}>{this.target}</Text>
@@ -50,10 +73,11 @@ export default class Game extends Component {
             />
           )}
         </View>
+        <Text>{gameStatus}</Text>
       </View>
     );
   }
-}
+}0
 
 const styles = StyleSheet.create({
   container: {
